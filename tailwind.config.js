debb2af89f536fcdb08ad3e80e8ae081cb38aae0
Richard Ten <rten04@icloud.com>
2023-11-02 08:53:28 -0400

    Add font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B1B2FF",
+        secondary: "#AAC4FF",
+        pink: "#EBC7E8",
+      },
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
