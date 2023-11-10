e87958630009a6d18de93d3f791ef88d6df52b6f
Richard Ten <rten04@icloud.com>
2023-11-10 18:27:21 +0000

    Add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import Benefits from "./components/Benefits/Benefits";
+import CtaBanner from "./components/CtaBanner";
+import FAQs from "./components/FAQs/FAQs";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Pricing from "./components/Pricing/Pricing";
+import RecentWork from "./components/RecentWork/RecentWork";
+import Testimonials from "./components/Testimonials/Testimonials";
+import ClientsCarousel from "./components/Clients/ClientsCarousel";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ClientsCarousel />
+      <Benefits />
+      <Testimonials />
+      <RecentWork />
+      <Pricing />
+      <FAQs />
+      <CtaBanner />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
